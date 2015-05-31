107c18288de7d48264389388b313f6eb6f68d4e1
NickDownie <nick.j.downie@gmail.com>
2015-05-31 15:17:06 -0700

    Second Commit
    Added the "updateCountdown()" method, to get the text from the
    editTexts, convert them to respected units of measure and then call them
    from the onClickListener method.
    
    ** Is not running, it considers the button to be null

--- Project/src/com/nickdown/ysera/Main.java
+++ Project/src/com/nickdown/ysera/Main.java
@@ -1,23 +1,54 @@
 package com.nickdown.ysera;
 
+import java.util.Calendar;
+
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Main extends Activity {
+	
+	public Button bt_calc;
+	public EditText et_day;
+	public EditText et_month;
+	public EditText et_year;
+	public TextView tv_result;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		bt_calc = (Button)findViewById(R.id.btncalc);
+		et_day = (EditText)findViewById(R.id.et_day);
+		et_month = (EditText)findViewById(R.id.et_month);
+		et_year = (EditText)findViewById(R.id.et_year);
+		tv_result = (TextView)findViewById(R.id.tv_result);
 
+		if(bt_calc == null){
+			Log.e("ERRRROOR", "Button is null");
+		}
+		
+		bt_calc.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				updateCountdown();
+			}
+			
+		});
+				
+		
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
@@ -60,5 +91,37 @@ public class Main extends Activity {
 			return rootView;
 		}
 	}
+	
+	private void updateCountdown(){
+		String day = et_day.getText().toString();
+		int d = Integer.parseInt(day);
+		
+		String month = et_month.getText().toString();
+		int m = Integer.parseInt(month);
+		
+		String year = et_year.getText().toString();
+		int y = Integer.parseInt(year);
+		
+		Calendar cal = Calendar.getInstance();
+		cal.set(y, m, d);
+		
+		long eventTime = cal.getTimeInMillis();
+
+		Calendar today = Calendar.getInstance();
+		long todayInMillis = today.getTimeInMillis();
+		
+		long diff = eventTime - todayInMillis;
+		
+		int secs = (int) (diff/1000);
+		int mins = (secs/60);
+		int hours = (mins/60);
+		int days = (hours/24);
+		int weeks = (days/7);
+		
+		tv_result.setText("Days: " + days + ", Hours: " + hours + ", Minutes: " + mins + ", Seconds: " + secs);
+	
+	}
+	
+	
 
 }
