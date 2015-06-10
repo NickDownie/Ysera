ca26308dd67e9f2b84f1437a2a83845116a7b02b
NickDownie <nick.j.downie@gmail.com>
2015-06-09 18:27:42 -0700

    timerTask updates

--- Project/src/com/nickdown/ysera/Main.java
+++ Project/src/com/nickdown/ysera/Main.java
@@ -1,11 +1,23 @@
+/*
+ * Tasks:
+ * 1) Need a continuous countdown
+ * --added a onTick(), but it is not working yet....needs attention
+ * --onFinish() will be needed as well, can be done after the tick is finished
+ * 
+ */
+
 package com.nickdown.ysera;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -16,20 +28,23 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main extends Activity {
 
+	final Handler handler = new Handler();
 	public static final long MIN = 60;
 	public static final long HOUR = 60 * MIN;
-	public static final long DAY = 24*HOUR;
-	
-	
+	public static final long DAY = 24 * HOUR;
+
 	public static Button bt_calc;
 	public static EditText et_day;
 	public static EditText et_month;
 	public static EditText et_year;
 	public static TextView tv_result;
-	
+
+	public Timer timer;
+	public TimerTask timerTask;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +59,54 @@ public class Main extends Activity {
 			getFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		startTimer();
+	}
+
+	public void startTimer() {
+		timer = new Timer();
+
+		initializeTimerTask();
+
+		timer.schedule(timerTask, 5000, 1000);
+	}
+
+	public void stopTimerTask(View w) {
+		if (timer != null) {
+			timer.cancel();
+			timer = null;
+		}
+	}
+
+	/*
+	 * NEEDS WORK!!!!!!
+	 */
+	public void initializeTimerTask(){
+		timerTask = new TimerTask(){
+			public void run(){
+				
+				handler.post(new Runnable){
+					public void run(){
+						
+						Calendar calendar = Calendar.getInstance();
+						SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd:MMMM:yyyy HH:mm:ss a");
+						final String strDate = simpleDateFormat.format(calendar.getTime());
+					
+						int duration = Toast.LENGTH_SHORT;
+						Toast toast = Toast.makeText(getApplicationContext(), strDate, duration);
+						toast.show();
+					}
+					
+				}
+			}
+		}
+			
 	}
 
 	@Override
@@ -101,7 +164,8 @@ public class Main extends Activity {
 
 	private static void updateCountdown() {
 		/*
-		 * Getting inputs and parsing/setting the input to new variables variable
+		 * Getting inputs and parsing/setting the input to new variables
+		 * variable
 		 */
 		String day = et_day.getText().toString();
 		int d = Integer.parseInt(day);
@@ -117,12 +181,12 @@ public class Main extends Activity {
 		 */
 		Calendar cal = Calendar.getInstance();
 		cal.clear();
-		cal.set(y, m-1, d);
+		cal.set(y, m - 1, d);
 		long eventTime = cal.getTimeInMillis();
-		
+
 		/*
-		 * Setting the current time to right now object, then converting it
-		 * to millis and setting it to todayInMillis
+		 * Setting the current time to right now object, then converting it to
+		 * millis and setting it to todayInMillis
 		 */
 		Calendar rightNow = Calendar.getInstance();
 		long todayInMillis = rightNow.getTimeInMillis();
@@ -130,27 +194,26 @@ public class Main extends Activity {
 		/*
 		 * Calculating the difference between event/current time
 		 */
-		long diffInMilliSec = eventTime - todayInMillis;//rightNow.getTimeInMillis();
+		long diffInMilliSec = eventTime - todayInMillis;// rightNow.getTimeInMillis();
 		long diffInSec = TimeUnit.MILLISECONDS.toSeconds(diffInMilliSec);
 
 		/*
 		 * Converting millis to ss/mm/hh/dd from the calculated difference
 		 */
-		int numDay = (int)(diffInSec / DAY);
+		int numDay = (int) (diffInSec / DAY);
 		diffInSec %= DAY;
-		int numHour = (int)(diffInSec/ HOUR);
+		int numHour = (int) (diffInSec / HOUR);
 		diffInSec %= HOUR;
-		int numMin =(int)(diffInSec / MIN);
-		int numSec = (int)(diffInSec % MIN);
-		
-		
-		
+		int numMin = (int) (diffInSec / MIN);
+		int numSec = (int) (diffInSec % MIN);
+
 		/*
 		 * Outputing the data
 		 */
-		tv_result.setText("Days: " + numDay + ", Hours: " + numHour + ", Minutes: "
-				+ numMin + ", Seconds: " + numSec);
-		//tv_result.setText(postTimeStamp);
-	}
 
+		tv_result.setText("Days: " + numDay + ", Hours: " + numHour
+				+ ", Minutes: " + numMin + ", Seconds: " + numSec
+				+ " remaining!");
+
+	}
 }
